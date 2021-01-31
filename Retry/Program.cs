e31d0a9034926c5a254b88e077915dd26c404a5e
Gela <Gela@LAPTOP-T1HDK1RI>
2021-01-31 20:49:58 +0400

    Add project files.

---
+++ Retry/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Retry
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var htmlFromGoogle = await Retryer.RetryAsync(
+                () => FetchHtmlAsync("http://www.google.com"),
+                TimeSpan.FromSeconds(2),
+                3);
+
+            Console.WriteLine("Html fetched from google");
+            Console.WriteLine(htmlFromGoogle);
+
+            Console.WriteLine();
+            Console.WriteLine("##########################");
+            Console.WriteLine();
+
+            var htmlFromIncorrectWebsite = await Retryer.RetryAsync(
+                () => FetchHtmlAsync("http://www.bla23.com/"),
+                TimeSpan.FromSeconds(2),
+                3); // It must fails
+
+            Console.WriteLine(htmlFromIncorrectWebsite);
+        }
+
+        public static async Task<string> FetchHtmlAsync(string address)
+        {
+            Console.WriteLine("Start fetching");
+
+            await Task.Delay(1000); //simulating delay
+            using var client = new HttpClient();
+            var result = await client.GetAsync(new Uri(address));
+            result.EnsureSuccessStatusCode();
+            return await result.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ Retry/Retryer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Retry
+{
+    public static class Retryer
+    {
+        public static async Task<T> RetryAsync<T>(Func<Task<T>> action, TimeSpan delay, int retryCount)
+        {
+            for (var i = 0; i < retryCount + 1; i++)
+            {
+                try
+                {
+                    return await action();
+                }
+                catch (Exception)
+                {
+                    if (i >= retryCount)
+                        throw;
+
+                    await Task.Delay(delay);
+                }
+            }
+
+            return default;
+        }
+    }
+}
